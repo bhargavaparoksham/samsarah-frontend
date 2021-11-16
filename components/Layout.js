6cb59de820adbaf8e0b7a3c86a7e2a7f2e63f80d
Ankit <ankitsawardekar0705@gmail.com>
2021-11-16 12:18:54 +0530

    Home page desktop

--- components/Layout.js
+++ components/Layout.js
@@ -4,95 +4,181 @@ import state from "../utils/state"; // Global state
 import NextNProgress from "nextjs-progressbar"; // Navigation progress bar
 import styles from "../styles/components/Layout.module.scss"; // Component styles
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon"; // Address avatar
+import { Container } from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+// import Links from '@mui/material/Link';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 export default function Layout({ children }) {
-  return (
-    <div>
-      {/* Navigation progress bar */}
-      <NextNProgress
-        color="#282846"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        options={{
-          showSpinner: false,
-        }}
-      />
+   return (
+      <div>
+         {/* Navigation progress bar */}
+         <NextNProgress
+            color="#282846"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={3}
+            options={{
+               showSpinner: false,
+            }}
+         />
 
-      {/* HTML Meta + Header */}
-      <Meta />
-      <Header />
-      <div className={styles.layout__content}>{children}</div>
-
-    </div>
-  );
+         {/* HTML Meta + Header */}
+         <Meta />
+         {/* <Header /> */}
+         <HeaderNav />
+         <div className={styles.layout__content}>{children}</div>
+         <Footer />
+      </div>
+   );
 }
 
 // HTML Head
 function Meta() {
-  return (
-    <Head>
-      {/* Primary Meta Tags */}
-      <title>Samsarah</title>
-      <meta name="title" content="Samsarah" />
-      <meta
-        name="description"
-        content="Samsarah is a financial social network"
-      />
-      {/* Google fonts */}
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-  );
+   return (
+      <Head>
+         {/* Primary Meta Tags */}
+         <title>Samsarah</title>
+         <meta name="title" content="Samsarah" />
+         <meta
+            name="description"
+            content="Samsarah is a financial social network"
+         />
+         {/* Google fonts */}
+         <link rel="preconnect" href="https://fonts.gstatic.com" />
+         <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+         />
+      </Head>
+   );
 }
 
 // Header (+ auth management)
 function Header() {
-  // Collect address and unlock function from global state
-  const { address, unlock } = state.useContainer();
+   // Collect address and unlock function from global state
+   const { address, unlock } = state.useContainer();
 
-  return (
-    <div className={styles.layout__header}>
-      <div className="sizer">
-        {/* Logo */}
-        <div>
-          <Link href="/">
-            <a>
-             <img src="logo.png" alt="logo" height="50" width="50" />
-            </a>
-          </Link>
-        </div> 
+   return (
+      <div className={styles.layout__header}>
+         <Container maxWidth="lg">
+            <div className="">
+               {/* Logo */}
+               <div>
+                  <Link href="/">
+                     <a>
+                        <img src="samsara-logo.svg" alt="logo" />
+                     </a>
+                  </Link>
+               </div>
 
-        {/* Auth button */}
-        <div>
-          <button onClick={unlock}>
-            {address ? (
-              // If authenticated
-              <>
-                {/* Render address */}
-                <span>
-                  {address.startsWith("0x")
-                    ? // If ETH address, render truncated address
-                      address.substr(0, 6) +
-                      "..." +
-                      address.slice(address.length - 4)
-                    : // Else, render ENS name
-                      address}
-                </span>
-                {/* Render avatar */}
-                <Jazzicon diameter={16} seed={jsNumberForAddress(address)} />
-              </>
-            ) : (
-              // Else, display Connect Wallet prompt
-              "Connect Wallet"
-            )}
-          </button>
-        </div>
+               {/* Auth button */}
+               <div>
+                  <button onClick={unlock}>
+                     {address ? (
+                        // If authenticated
+                        <>
+                           {/* Render address */}
+                           <span>
+                              {address.startsWith("0x")
+                                 ? // If ETH address, render truncated address
+                                 address.substr(0, 6) +
+                                 "..." +
+                                 address.slice(address.length - 4)
+                                 : // Else, render ENS name
+                                 address}
+                           </span>
+                           {/* Render avatar */}
+                           <Jazzicon diameter={16} seed={jsNumberForAddress(address)} />
+                        </>
+                     ) : (
+                        // Else, display Connect Wallet prompt
+                        <img src="wallet.svg" alt="" />
+                     )}
+                  </button>
+               </div>
+            </div>
+         </Container>
       </div>
-    </div>
-  );
+   );
+}
+
+function HeaderNav() {
+   const { address, unlock } = state.useContainer();
+   return (
+      <nav className={styles.layout__header}>
+         <Container>
+            <div className={styles.navbar}>
+               <Link href="/">
+                  <a>
+                     <img src="samsara-logo.svg" alt="logo" />
+                  </a>
+               </Link>
+               <div className={styles.searchbar_wrapper}>
+                  <input type="text" placeholder="Search any ENS name or Ethereum address" className={styles.searchbar} />
+               </div>
+               <button onClick={unlock}>
+                  {address ? (
+                     // If authenticated
+                     <>
+                        {/* Render address */}
+                        <span>
+                           {address.startsWith("0x")
+                              ? // If ETH address, render truncated address
+                              address.substr(0, 6) +
+                              "..." +
+                              address.slice(address.length - 4)
+                              : // Else, render ENS name
+                              address}
+                        </span>
+                        {/* Render avatar */}
+                        <Jazzicon diameter={16} seed={jsNumberForAddress(address)} />
+                     </>
+                  ) : (
+                     // Else, display Connect Wallet prompt
+                     <img src="wallet.svg" alt="" />
+                  )}
+               </button>
+            </div>
+         </Container>
+      </nav>
+   );
+}
+
+
+function Footer() {
+   return (
+      <footer className={styles.footer}>
+         <Container>
+            <div className={styles.footer_wrapper}>
+               <div>
+                  <img src="samsara-logo-white.png" />
+                  <span>Samsarah</span>
+               </div>
+               <div>
+                  <ul>
+                     <li>
+                        <Link href="/">
+                           <img src="logo-1.png" alt="" />
+                        </Link>
+                     </li>
+                     <li>
+                        <Link href="/">
+                           <img src="twitter.png" alt="" />
+                        </Link>
+                     </li>
+                     <li>
+                        <Link href="/">
+                           <img src="logo-3.png" alt="" />
+                        </Link>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </Container>
+      </footer>
+   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import { toast } from "react-toastify"; // Toast notifications
 import Loader from "react-loader-spinner"; // Spinner
 import { useEffect, useState, useLayoutEffect } from "react"; // State management
 import styles from "../styles/pages/Home.module.scss"; // Page styles
-import Image from 'next/image'
+import Image from 'next/image';
+import Container from '@material-ui/core/Container';
+import { style } from "dom-helpers";
 
 
 export default function Home() {
@@ -25,21 +27,73 @@ export default function Home() {
  */
 function Main() {
   return (
+    // <div>
+    //   <div className={styles.main}>
+    //     <div className={styles.main_content}>
+    //       <h1>Samsarah</h1>
+    //       <h4>The financial social network</h4>
+    //       <div className={styles.main_buttons}>
+    // <Link href="/profile">
+    //   <button>
+    //     Create Profile
+    //   </button>
+    // </Link>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
     <div>
-    <div className={styles.main}>
-       <div className={styles.main_content}>
-        <h1>Samsarah</h1>
-        <h4>The financial social network</h4>
-        <div className={styles.main_buttons}>
+      <HeroSection />
+      <CreateJoinSection />
+
+    </div>
+  );
+
+}
+
+function HeroSection() {
+  return (
+    <section className={styles.home_wrapper}>
+      <div className={styles.home_wrapper_sec}>
+        <div className={styles.hero_banner}></div>
+      </div>
+      <div className={styles.home_wrapper_right_sec}>
+        <h2>Samsarah</h2>
+        <p>
+          Join the financial social netowrk<br /> built on Ethereum
+        </p>
+        <Link href="/profile">
+          <button className={styles.CTA}>
+            Create Profile
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+function CreateJoinSection() {
+  return (
+    <section className={styles.section_create_join}>
+      <Container maxWidth="lg">
+        <div className={styles.create_join_wrapper}>
+          <div className={`${styles.create_join_wrapper_sec} ${styles.create_join_wrapper_left}`}>
+            <h2>Create or Join a <br /> DAO
+            </h2>
+            <p>
+              Creating  or joining a DAO is now as <span><br /></span> easy as creating a facebook group.Check out what DAO’s <span><br /></span> your friends are part of & apply to join them.
+            </p>
             <Link href="/profile">
-              <button>
+              <button className={styles.CTA}>
                 Create Profile
               </button>
-            </Link> 
-        </div>  
-      </div>
-     </div>
-     </div>
+            </Link>
+          </div>
+          <div className={`${styles.create_join_wrapper_sec} ${styles.create_join_wrapper_banner}`}>
+            <img src="creat-join-sec.png" alt="" />
+          </div>
+        </div>
+      </Container>
+    </section>
   );
-    
 }
